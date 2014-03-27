dd0f4bd8af649facb541a0a07721a5a0a23b8d34
Henok Getachew <henygetachew@gmail.com>
2014-03-28 02:57:52 +0300

    Load the contact from the database or create it if it doesn't exist.

--- rapidsms_relay/views.py
+++ rapidsms_relay/views.py
@@ -7,10 +7,12 @@ from rapidsms_relay import settings
 def index(request):
     phone_number = request.GET.get('phone_number','')
     message = request.GET.get('message','')
+        
+    sent_identity = phone_number
+
+    backend_to_use = Backend.objects.get(name=settings.RAPIDSMS_BACKENDS_FOR_SMS_RELAY)
     
-    conn = Connection()
-    conn.identity = phone_number
-    conn.backend = Backend.objects.get(name=settings.RAPIDSMS_BACKENDS_FOR_SMS_RELAY)
+    conn, created = Connection.objects.get_or_create(identity=sent_identity, backend = backend_to_use)
     
     send(message, conn)
     
