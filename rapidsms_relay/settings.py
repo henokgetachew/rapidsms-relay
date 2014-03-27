34a63fc80ec1676af5c29817975cf9a3f8fe42d2
Henok Getachew <henygetachew@gmail.com>
2014-03-27 04:23:29 +0300

    Local settings file added

--- rapidsms_relay/settings.py
+++ rapidsms_relay/settings.py
@@ -1,7 +1,5 @@
 
-#Settings for elmis-relay
-RAPIDSMS_BACKENDS_FOR_SMS_RELAY = "elmis_tropo_backend"
-INCOMING_MESSAGES_RELAY_EVERYTHING = "True"
-#If the relay everything is false, then define a prefix
-INCOMING_MESSAGES_RELAY_INDICATOR_SMS_PREFIX = "elmis"
-INCOMING_MESSAGES_RELAY_URL = "http://localhost:9091/sms"
+try:
+    from localsettings import *
+except ImportError:
+    pass
