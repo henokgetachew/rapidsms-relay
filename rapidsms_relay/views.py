6bd6805bc2ccf9e8a11196551c2db82c665d0aac
Henok <henygetachew@gmail.com>
2014-05-15 19:08:06 +0300

    Enable routing to multiple backends

--- rapidsms_relay/views.py
+++ rapidsms_relay/views.py
@@ -4,13 +4,23 @@ from rapidsms.router import send
 from rapidsms.models import Connection, Backend
 from rapidsms_relay import settings
 
+def choose_backend (phone_no):
+    matching_dict = settings.RAPIDSMS_BACKENDS_COUNTRY_CODE_MATCHING_FOR_SMS_RELAY
+    backend = None
+    for key in matching_dict.keys():
+        if (phone_no.startswith(key)):
+            backend = matching_dict[key]
+    backend = matching_dict['default']
+    return Backend.objects.get(name = backend)      
+        
+
 def index(request):
     phone_number = request.GET.get('phone_number','')
     message = request.GET.get('message','')
         
     sent_identity = phone_number
 
-    backend_to_use = Backend.objects.get(name=settings.RAPIDSMS_BACKENDS_FOR_SMS_RELAY)
+    backend_to_use = choose_backend(phone_number)
     
     conn, created = Connection.objects.get_or_create(identity=sent_identity, backend = backend_to_use)
     
